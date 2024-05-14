c0bc53fee1e5326a5515a22406e8644b57952834
Prado Matias Santiago <matias14prado@gmail.com>
2024-05-14 12:19:01 -0300

    se completo el ejecicio 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.Write("Hello, World!");
+
+static int invertirNumero(int numero)
+{
+    int aux;
+    int numeroInvertido=0;
+    while(numero != 0)
+    {
+        aux = numero % 10;
+        numeroInvertido = (numeroInvertido * 10) + aux;
+        numero = numero / 10; 
+    }
+    return numeroInvertido;
+}
+Console.WriteLine("Ingrese el numero que desea invertir");
+string n=Console.ReadLine();
+int numero;
+if(int.TryParse(n, out numero))
+{  
+    if(numero > 0)
+    {
+        int numeroInvertido = invertirNumero(numero);
+        Console.WriteLine("El numero invertido es: "+ numeroInvertido);
+    }else
+    {
+        Console.WriteLine("ERROR EL NUMERO INGRESADO NO ES MAYOR A 0");
+    }
+    
+}else
+{
+    Console.WriteLine("ERROR EL VALOR INGRESADO NO ES UN NUMERO");
+}
